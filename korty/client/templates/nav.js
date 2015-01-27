c196e55d7c037152591f47f2b09f8dbc6b8b4c59
Michał <ludwinio92@gmail.com>
2015-01-27 21:18:43 +0100

    tomek fiutek

--- korty/client/templates/nav.js
+++ korty/client/templates/nav.js
@@ -23,6 +23,6 @@ Template.nav.events({
     'click #logout': function (e) {
         e.preventDefault();
         Meteor.logout();
-        Router.go('wilkomen');
+        Router.go('welp');
     }
 });
